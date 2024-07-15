44a3ee2932782d4d7de006f83c8a480758a9a424
Younus <you@example.com>
2024-07-15 10:04:28 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,91 +1,99 @@
 let gameSeq = [];
 let userSeq = [];
-let btns = ["red","blue","yellow","green"];
+let btns = ["red", "blue", "yellow", "green"];
 let start = false;
 let level = 0;
 let high = 0;
 let h2 = document.querySelector("h2");
 let h1 = document.querySelector("h1");
- document.addEventListener("keypress", function(){
-  h1.innerText = "Simon Game"
-    if(start == false){
-        console.log("Game started");
-        start = true;
-        document.querySelector("body").style.backgroundColor = "white";
-        levelUp();
-    }
- });
+let start1 = document.querySelector("#start");
+let reload = document.querySelector("#reload");
 
- function btnFlash(btn) {
-   btn.classList.add("flash");
-   setTimeout(function () {
-      btn.classList.remove("flash");
-   }, 1000);
- }
- 
- function levelUp() {
-    userSeq = [];
-    console.log(userSeq);
-    level++;
-    h2.innerText = `level ${level}`;
-    
-    let randIndx = Math.floor(Math.random() * 4);
-    let randColor = btns[randIndx];
-    let randBtn = document.querySelector(`.${randColor}`);
-    btnFlash(randBtn);
-    gameSeq.push(randColor);
-    console.log(gameSeq);
- }
- function checkAns(idx) {
-  if(userSeq[idx] === gameSeq[idx]) {
-    if(userSeq.length == gameSeq.length) 
-      setTimeout(levelUp, 300);
+reload.addEventListener("click",()=>{
+  // Reloads the current page
+location.reload();
+})
+
+start1.addEventListener("click", () => {
+  starting();
+});
+
+function starting() {
+  h1.innerText = "Simon Game";
+  if (!start) {
+    console.log("Game started");
+    start = true;
+    document.querySelector("body").style.backgroundColor = "white";
+    levelUp();
   }
-     else {
-      h1.innerHTML = `Game Over! <b> Your Score Was ${level} </b>`;
-    h2.innerText = "press any key to start again!";
-    document.querySelector("body").style.backgroundColor = "#e74c3ea8";
+}
+
+function btnFlash(btn) {
+  btn.classList.add("flash");
+  setTimeout(function () {
+    btn.classList.remove("flash");
+  }, 1000);
+}
+
+function levelUp() {
+  userSeq = [];
+  console.log(userSeq);
+  level++;
+  h2.innerText = `Level ${level}`;
+
+  let randIndx = Math.floor(Math.random() * 4);
+  let randColor = btns[randIndx];
+  let randBtn = document.querySelector(`.${randColor}`);
+  btnFlash(randBtn);
+  gameSeq.push(randColor);
+  console.log(gameSeq);
+}
+
+function checkAns(idx) {
+  if (userSeq[idx] === gameSeq[idx]) {
+    if (userSeq.length === gameSeq.length) {
+      setTimeout(levelUp, 300);
+    }
+  } else {
+    h1.innerHTML = `Game Over! <b>Your Score Was ${level}</b>`;
+    h2.innerText = "Press the Start button to play again!";
+    document.querySelector("body").style.backgroundColor = "#ff0000";
+    document.querySelector("body").style.color = "white";
     reset();
   }
- }
+}
 
- function allBtn() {
-   console.log("button was pressed");
-   let btn = this;
-   UserBtnFlash(btn);
-   let userColor = btn.getAttribute("id");
-   userSeq.push(userColor);
-   checkAns(userSeq.length-1);
- } 
+function allBtn() {
+  console.log("Button was pressed");
+  let btn = this;
+  UserBtnFlash(btn);
+  let userColor = btn.getAttribute("id");
+  userSeq.push(userColor);
+  checkAns(userSeq.length - 1);
+}
 
- let allBtns = document.querySelectorAll(".box");
- for(btn of allBtns) {
-   btn.addEventListener("click", allBtn)
- }
+let allBtns = document.querySelectorAll(".box");
+for (let btn of allBtns) {
+  btn.addEventListener("click", allBtn);
+}
 
- function UserBtnFlash(btn) {
+function UserBtnFlash(btn) {
   btn.classList.add("Userflash");
   setTimeout(function () {
     btn.classList.remove("Userflash");
   }, 300);
- }
+}
 
- function reset() {
+function reset() {
   start = false;
   userSeq = [];
   gameSeq = [];
   highscore();
   level = 0;
- }
- function highscore() {
-  if(level>high) high = level;
-  let inner = document.querySelector("h3");
-  if(high == level) {
-    inner.innerHTML = `High Score = ${level}`; 
-  }
-  else {
-    inner.innerHTML = `New High Score = ${high}`;
-  }
- }
+}
 
- 
+function highscore() {
+  if (level > high) high = level;
+  let inner = document.querySelector("h3");
+  inner.innerHTML = `High Score = ${high}`;
+}
